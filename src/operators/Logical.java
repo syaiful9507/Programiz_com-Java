e3208d887bfc8300f4b46680e45e1b3879f09bbe
SYAIFUL <syaiful9507@gmail.com>
2022-09-25 23:14:58 +0700

    operators

---
+++ src/operators/Logical.java
@@ -0,0 +1,41 @@
+package operators;
+/*
+Logical operators are used to check whether an expression is true or false.
+They are used in decision making.
+ */
+public class Logical {
+    public static void main(String[] args) {
+        // && operator
+        System.out.println( (5 > 3) && (8 > 5)); //true
+        System.out.println( (5 > 3) && (8 < 5)); //false
+
+        // || operator
+        System.out.println( (5 < 3) || (8 > 5)); //true
+        System.out.println( (5 > 3) || (8 < 5)); //true
+        System.out.println( (5 < 3) || (8 < 5)); //false
+
+        // !operator
+        System.out.println( !(5 == 3) ); //true
+        System.out.println( !(5 > 3)); //false
+
+        /*
+
+        -------------------------------------------------------------
+        Working of Program
+
+        (5 > 3) && (8 > 5) returns true because both (5 > 3) and (8 > 5) are true.
+        (5 > 3) && (8 < 5) returns false because the expression (8 < 5) is false.
+        (5 < 3) || (8 > 5) returns true because the expression (8 > 5) is true.
+        (5 > 3) || (8 < 5) returns true because the expression (5 > 3) is true.
+        (5 < 3) || (8 < 5) returns false because both (5 < 3) and (8 < 5) are false.
+        !(5 == 3) returns true because 5 == 3 is false.
+        !(5 > 3) returns false because 5 > 3 is true.
+
+         */
+
+
+
+
+
+    }
+}
--- src/operators/Relational.java
+++ src/operators/Relational.java
@@ -12,6 +12,27 @@ a < b;
  */
 public class Relational {
     public static void main(String[] args) {
+        //create variables
+        int a = 7, b = 11;
 
+        //value of a and b\
+        System.out.println("a is : " + a + " and b is " + b);
+
+        // == operator
+        System.out.println(a == b); //false
+        // != operator
+        System.out.println(a != b); //true
+        // > operator
+        System.out.println(a > b); //false
+        // < operator
+        System.out.println(a < b); //true
+        // => operator
+        System.out.println(a >= b); //false
+        // <= operator
+        System.out.println(a <= b); //true
+
+        /*
+        Note: Relational operators are used in decision making and loops.
+         */
     }
 }
