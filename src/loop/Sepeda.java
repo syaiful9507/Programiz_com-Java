eb57008c4b2f1dff6dec324ee71f566e54e8046f
SYAIFUL <syaiful9507@gmail.com>
2022-10-25 22:36:13 +0700

    sepeda

--- src/loop/Sepeda.java
+++ src/loop/Sepeda.java
@@ -5,25 +5,36 @@ public class Sepeda {
         int number = 20;
         String text = "SEPEDA";
         int lg = text.length();
-        int rs = 0;
+        int rs = number - lg; // rs = 20 - 6 = 14 - initial
         int i = 0;
 
-        System.out.println(lg);
         for (i = i; i < lg; i++) {
             System.out.print(text.charAt(i));
-            rs = number - lg;
-            if (i == 5 && lg < rs){
-                lg = lg;
+            //System.out.println(" ini value i : " + i);
+            // rs = number - i;  # 1. rs = 20 - 5 = 15 |  # 2. rs = 9 - 6 = 2
+            if ((i == lg - 1) && (rs > lg)){ // 14 > 6 -> 8 > 6
+                System.out.print(" <-- Bagian i ");
                 i = -1;
                 for (int j = 0; j < lg ; j++) {
                     System.out.print(text.charAt(j));
+                    //System.out.println(" ini value i setelah j : " + i);
                 }
-            } else if ( i == 5 && lg > rs) {
+                System.out.print(" <-- Bagian j " + rs + " ");
+                rs = rs - lg; // 14 - 6 = 8
+                //lg = rs;
+
+
+            } else if (rs <= lg ) {
+                System.out.print(" <-- Bagian i ");
                 lg = rs;
                 i  = -1;
-                for (int j = 0; j < lg; j++) {
-                    System.out.print(text.charAt(j));
+                for (int k = 0; k < lg; k++) {
+                    System.out.print(text.charAt(k));
+                    //System.out.println(" ini value i setelah k : " + i);
                 }
+                System.out.print(" <-- Bagian k ");
+                break;
+
             }
         }
     }
