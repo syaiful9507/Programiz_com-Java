f6d69605b00c7dd6e1d0a93916f57a66fc209077
SYAIFUL <syaiful9507@gmail.com>
2022-11-03 01:37:27 +0700

    refactor packages

---
+++ src/challenges/string/FindLongerString.java
@@ -0,0 +1,19 @@
+package challenges.string;
+
+import java.util.Scanner;
+
+public class FindLongerString {
+    String countString(String txt1, String txt2){
+        String countStr = (txt1.length() > txt2.length()) ? txt1 : txt2;
+        return countStr;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String text1 = in.nextLine();
+        String text2 = in.nextLine();
+        FindLongerString objStr = new FindLongerString();
+        String resultString = objStr.countString(text1, text2);
+        System.out.println(resultString);
+    }
+}
