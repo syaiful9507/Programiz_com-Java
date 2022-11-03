dfb2b9e8f07647afeb6575d6b832fe144f4b3f60
SYAIFUL <syaiful9507@gmail.com>
2022-11-03 12:52:30 +0700

    Mean Height of Players

---
+++ src/challenges/array/MeanHeightPlayers.java
@@ -0,0 +1,19 @@
+package challenges.array;
+
+import java.util.Scanner;
+
+public class MeanHeightPlayers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double[] heights = new double[11];
+        double sum = 0;
+        double mean = 0;
+        for (int i = 0; i < heights.length ; i++) {
+            heights[i] = in.nextDouble();
+            sum = sum + heights[i];
+        }
+        mean = sum / heights.length;
+        System.out.println(mean);
+    }
+
+}
