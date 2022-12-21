3e6f97e0340c18b5c63b3017e5c758392492ddb2
SYAIFUL <syaiful9507@gmail.com>
2022-12-21 10:35:36 +0700

    Access Static Properties within Class

---
+++ src/challenges/AccessStaticPropertiesWithinClass.java
@@ -0,0 +1,39 @@
+package challenges;
+/*
+Challenge:
+Access Static Properties within Class (Easy)
+
+Problem Description
+#Create a program to access the static field and method within the class.
+* Create a class named College with a static field name and static method display().
+* Inside the method, print the value of name.
+* Create a main() method.
+* Inside the method, get a string input from the user and assign it to the inputName variable.
+* Initialize the static field name with the value inputName.
+* Call the static method display().
+
+Example
+Test Input
+MIT
+
+Expected Output
+MIT
+ */
+
+import java.util.Scanner;
+
+class College {
+    static String name;
+    static void display(){
+        System.out.println(name);
+    }
+}
+public class AccessStaticPropertiesWithinClass {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String inputName = in.nextLine();
+        College.name = inputName;
+        College.display();
+
+    }
+}
